53a5fe4b18a02d45976e8a8281e12d490b0131f8
Jb Evain <jbevain@gmail.com>
2005-02-16 16:26:49 +0000

    make Mono.Xml.* internal
    svn path=/trunk/cecil/; revision=40758

--- lib/Mono.Xml/SecurityParser.cs
+++ lib/Mono.Xml/SecurityParser.cs
@@ -2,7 +2,7 @@
 // Mono.Xml.SecurityParser.cs class implementation
 //
 // Author:
-//	Sebastien Pouliot (spouliot@motus.com)
+//  Sebastien Pouliot (spouliot@motus.com)
 //
 // (C) 2003 Motus Technologies Inc. (http://www.motus.com)
 //
@@ -17,10 +17,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -36,84 +36,77 @@ using System.Security;
 
 namespace Mono.Xml {
 
-	// convert an XML document into SecurityElement objects
-#if INSIDE_CORLIB
-	internal
-#else
-	[CLSCompliant(false)]    
-	public
-#endif
-	class SecurityParser : MiniParser, MiniParser.IHandler, MiniParser.IReader {
+    internal class SecurityParser : MiniParser, MiniParser.IHandler, MiniParser.IReader {
 
-		private SecurityElement root;
+        private SecurityElement root;
 
-		public SecurityParser () : base () 
-		{
-			stack = new Stack ();
-		}
+        public SecurityParser () : base ()
+        {
+            stack = new Stack ();
+        }
 
-		public void LoadXml (string xml) 
-		{
-			root = null;
-			xmldoc = xml;
-			pos = 0;
-			stack.Clear ();
-			Parse (this, this);
-		}
+        public void LoadXml (string xml)
+        {
+            root = null;
+            xmldoc = xml;
+            pos = 0;
+            stack.Clear ();
+            Parse (this, this);
+        }
 
-		public SecurityElement ToXml () 
-		{
-			return root;
-		}
+        public SecurityElement ToXml ()
+        {
+            return root;
+        }
 
-		// IReader
+        // IReader
 
-		private string xmldoc;
-		private int pos;
+        private string xmldoc;
+        private int pos;
 
-		public int Read () 
-		{
-			if (pos >= xmldoc.Length)
-				return -1;
-			return (int) xmldoc [pos++];
-		}
+        public int Read ()
+        {
+            if (pos >= xmldoc.Length)
+                return -1;
+            return (int) xmldoc [pos++];
+        }
 
-		// IHandler
+        // IHandler
 
-		private SecurityElement current;
-		private Stack stack;
+        private SecurityElement current;
+        private Stack stack;
 
-		public void OnStartParsing (MiniParser parser) {}
+        public void OnStartParsing (MiniParser parser) {}
 
-		public void OnStartElement (string name, MiniParser.IAttrList attrs) 
-		{
-			SecurityElement newel = new SecurityElement (name); 
-			if (root == null) {
-				root = newel;
-				current = newel;
-			}
-			else {
-				SecurityElement parent = (SecurityElement) stack.Peek ();
-				parent.AddChild (newel);
-			}
-			stack.Push (newel);
-			current = newel;
-			// attributes
-			int n = attrs.Length;
-			for (int i=0; i < n; i++)
-				current.AddAttribute (attrs.GetName (i), attrs.GetValue (i));
-		}
+        public void OnStartElement (string name, MiniParser.IAttrList attrs)
+        {
+            SecurityElement newel = new SecurityElement (name);
+            if (root == null) {
+                root = newel;
+                current = newel;
+            }
+            else {
+                SecurityElement parent = (SecurityElement) stack.Peek ();
+                parent.AddChild (newel);
+            }
+            stack.Push (newel);
+            current = newel;
+            // attributes
+            int n = attrs.Length;
+            for (int i=0; i < n; i++)
+                current.AddAttribute (attrs.GetName (i), attrs.GetValue (i));
+        }
 
-		public void OnEndElement (string name) 
-		{
-			current = (SecurityElement) stack.Pop ();
-		}
+        public void OnEndElement (string name)
+        {
+            current = (SecurityElement) stack.Pop ();
+        }
 
-		public void OnChars (string ch) 
-		{
-			current.Text = ch;
-		}
+        public void OnChars (string ch)
+        {
+            current.Text = ch;
+        }
 
-		public void OnEndParsing (MiniParser parser) {}
-	}
+        public void OnEndParsing (MiniParser parser) {}
+    }
 }
