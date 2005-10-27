81ebe78c2d009986106cef57f3e1e77c571daf31
Jb Evain <jbevain@gmail.com>
2005-10-27 08:02:39 +0000

    2005-10-27  Jb Evain  <jbevain@gmail.com>
    	* Mono.Cecil/GenericParamAttributes.cs:
    		Add new constraints flags.
    
    svn path=/trunk/cecil/; revision=52264

--- lib/Mono.Cecil/GenericParamAttributes.cs
+++ lib/Mono.Cecil/GenericParamAttributes.cs
@@ -35,6 +35,11 @@ namespace Mono.Cecil {
 		VarianceMask	= 0x0003,
 		NonVariant		= 0x0000,
 		Covariant		= 0x0001,
-		Contravariant	= 0x0002
+		Contravariant	= 0x0002,
+
+		SpecialConstraintMask			= 0x001c,
+		ReferenceTypeConstraint			= 0x0004,
+		NotNullableValueTypeConstraint	= 0x0008,
+		DefaultConstructorConstrait		= 0x0010
 	}
 }
