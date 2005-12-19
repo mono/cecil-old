73b3218fc15cf8cc5b149b7756721594e71fd241
Rodrigo B. de Oliveira <rbo@acm.org>
2005-12-19 13:35:28 +0000

    more test cases
    svn path=/trunk/cecil/; revision=54599

--- flowanalysis/Cecil.FlowAnalysis.Tests/ActionFlowTestFixture.cs
+++ flowanalysis/Cecil.FlowAnalysis.Tests/ActionFlowTestFixture.cs
@@ -33,6 +33,19 @@ using NUnit.Framework;
 namespace Cecil.FlowAnalysis.Tests {
 	[TestFixture]
 	public class ActionFlowTestFixture : AbstractFlowAnalysisTestFixture {
+		
+		[Test]
+		public void IntPropertyEquals1 ()
+		{
+			RunTestCase ("IntPropertyEquals1");
+		}
+		
+		[Test]
+		public void IntPropertyEquals2 ()
+		{
+			RunTestCase ("IntPropertyEquals2");
+		}
+		
 		[Test]
 		public void FloatGreaterThan ()
 		{
