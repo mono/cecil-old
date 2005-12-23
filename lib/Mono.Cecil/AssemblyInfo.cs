8292d7dacfd5d3dae36f1efa827bedbdef133aec
Jb Evain <jbevain@gmail.com>
2005-12-23 22:37:40 +0000

    2005-12-23  Jb Evain  <jbevain@gmail.com>
    	* Mono.Cecil/AssemblyInfo.cs:
    		Version 0.2 to be released.
    
    svn path=/trunk/cecil/; revision=54808

--- lib/Mono.Cecil/AssemblyInfo.cs
+++ lib/Mono.Cecil/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant (false)]
 [assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion ("0.1.1.1")]
+[assembly: AssemblyVersion ("0.2.0.0")]
 
