5cb58baf0d7907e1c0bf3e35ee59cf661dcde03f
Jb Evain <jbevain@gmail.com>
2006-02-07 21:52:18 +0000

    bump version after tag
    svn path=/trunk/cecil/; revision=56643

--- lib/Mono.Cecil/AssemblyInfo.cs
+++ lib/Mono.Cecil/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant (false)]
 [assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion ("0.3.0.0")]
+[assembly: AssemblyVersion ("0.3.0.1")]
 
