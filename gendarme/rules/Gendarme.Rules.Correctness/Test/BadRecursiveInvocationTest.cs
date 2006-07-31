a35b00bb0c6a0fcd2f452a156408840f702cf2c0
Sebastien Pouliot <sebastien@ximian.com>
2006-07-31 14:31:49 +0000

    2006-07-31  Sebastien Pouliot  <sebastien@ximian.com>
    	* BadRecursiveInvocationTest.cs: Convert (and add) tests to NUnit format.
    
    svn path=/trunk/cecil/; revision=63183

--- gendarme/rules/Gendarme.Rules.Correctness/Test/BadRecursiveInvocationTest.cs
+++ gendarme/rules/Gendarme.Rules.Correctness/Test/BadRecursiveInvocationTest.cs
@@ -1,17 +1,153 @@
-class BadRec {
+//
+// Unit tests for BadRecursiveInvocationRule
+//
+// Authors:
+//	Aaron Tomb <atomb@soe.ucsc.edu>
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2005 Aaron Tomb
+// Copyright (C) 2006 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 
-    /* This should be an error. */
-    public int Foo {
-        get { return Foo; }
-    }
+using System;
+using System.Reflection;
 
-    /* This should be fine, as it uses 'base.' */
-    public override int GetHashCode()
-    {
-        return base.GetHashCode();
-    }
+using Gendarme.Framework;
+using Gendarme.Rules.Correctness;
+using Mono.Cecil;
+using NUnit.Framework;
 
-    public static void Main(string[] args)
-    {
-    }
+namespace Test.Rules.Correctness {
+
+	[TestFixture]
+	public class BadRecursiveInvocationTest {
+
+		class BadRec {
+
+			/* This should be an error. */
+			public int Foo {
+				get { return Foo; }
+			}
+
+			/* correct */
+			public int Bar {
+				get { return -1; }
+			}
+
+			/* a more complex recursion */
+			public int FooBar {
+				get { return BarFoo; }
+			}
+
+			public int BarFoo {
+				get { return FooBar; }
+			}
+
+			/* This should be fine, as it uses 'base.' */
+			public override int GetHashCode ()
+			{
+				return base.GetHashCode ();
+			}
+			
+			/* not fine, missing 'base.' */
+			public override bool Equals (object o)
+			{
+				return Equals (o);
+			}
+			
+			public static int Fibonacci (int n)
+			{
+				if (n < 2)
+					return n;
+					
+				return Fibonacci (n - 1) + Fibonacci (n - 2);
+			}
+		}
+		
+		private IMethodRule rule;
+		private IAssemblyDefinition assembly;
+		private ITypeDefinition type;
+		private IModuleDefinition module;
+
+		[TestFixtureSetUp]
+		public void FixtureSetUp ()
+		{
+			string unit = Assembly.GetExecutingAssembly ().Location;
+			assembly = AssemblyFactory.GetAssembly (unit);
+			module = assembly.MainModule;
+			type = module.Types["Test.Rules.Correctness.BadRecursiveInvocationTest/BadRec"];
+			rule = new BadRecursiveInvocationRule ();
+		}
+
+		private IMethodDefinition GetTest (string name)
+		{
+			foreach (IMethodDefinition method in type.Methods) {
+			Console.WriteLine (method.Name);
+				if (method.Name == name)
+					return method;
+			}
+			return null;
+		}
+
+		[Test]
+		public void RecursiveProperty ()
+		{
+			IMethodDefinition method = GetTest ("get_Foo"); 
+			Assert.IsNotNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+		
+		[Test]
+		public void Property ()
+		{
+			IMethodDefinition method = GetTest ("get_Bar"); 
+			Assert.IsNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+
+		[Test, Ignore ("uncatched by rule")]
+		public void IndirectRecursiveProperty ()
+		{
+			IMethodDefinition method = GetTest ("get_FooBar"); 
+			Assert.IsNotNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+
+		[Test]
+		public void OverriddenMethod ()
+		{
+			IMethodDefinition method = GetTest ("GetHashCode"); 
+			Assert.IsNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+		
+		[Test]
+		public void BadRecursiveMethod ()
+		{
+			IMethodDefinition method = GetTest ("Equals"); 
+			Assert.IsNotNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+		
+		[Test]
+		public void Fibonacci ()
+		{
+			IMethodDefinition method = GetTest ("Fibonacci"); 
+			Assert.IsNull (rule.CheckMethod (assembly, module, type, method, new MinimalRunner()));
+		}
+	}
 }
