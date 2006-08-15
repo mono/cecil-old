12551b051dbabac50fae6dd2460cde6b1eb29c13
Sebastien Pouliot <sebastien@ximian.com>
2006-08-15 12:53:39 +0000

    2006-08-15  Russell Morris  <russell@russellsprojects.com>
    	* DontDestroyStackTrace.cs: Fix possible NRE when processing invalid 
    	IL. http://blog.russellsprojects.com/?p=3
    
    
    svn path=/trunk/cecil/; revision=63756

--- gendarme/rules/Gendarme.Rules.Exceptions/DontDestroyStackTrace.cs
+++ gendarme/rules/Gendarme.Rules.Exceptions/DontDestroyStackTrace.cs
@@ -161,10 +161,16 @@ namespace Gendarme.Rules.Exceptions {
 			case StackBehaviour.Popref_popi_popref:
 				return 3;
 			case StackBehaviour.Varpop:
-				// We have to determine from the call how many arguments will
-				// be popped from the stack
-				MethodReference callMethod = (MethodReference)instr.Operand;
-				return callMethod.Parameters.Count;
+				if(instr.Operand is MethodReference) {
+					// We have to determine from the call how many arguments will
+					// be popped from the stack
+					MethodReference callMethod = (MethodReference)instr.Operand;
+					return callMethod.Parameters.Count;
+				} else {
+					throw new InvalidOperationException("Unexpected instruction: '" + 
+					instr.OpCode.ToString() + "' at offset 0x" + 
+					instr.Offset.ToString("X"));
+				}
 			}
 			
 			return 0;
