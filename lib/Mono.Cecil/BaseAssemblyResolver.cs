ea48a955663327859a993fce0b7d71ba5e0b5851
Jb Evain <jbevain@gmail.com>
2006-08-17 20:55:17 +0000

    2006-08-17  Jb Evain  <jbevain@gmail.com>
    	* Mono.Cecil/BaseAssemblyResolver.cs:
    		Resolve the corlib depending on its version.
    
    svn path=/trunk/cecil/; revision=63944

--- lib/Mono.Cecil/BaseAssemblyResolver.cs
+++ lib/Mono.Cecil/BaseAssemblyResolver.cs
@@ -26,72 +26,106 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-namespace Mono.Cecil
-{
+namespace Mono.Cecil {
 
 	using System;
 	using System.IO;
+	using SR = System.Reflection;
 	using System.Text;
 
-	public abstract class BaseAssemblyResolver : IAssemblyResolver
-	{
+	public abstract class BaseAssemblyResolver : IAssemblyResolver {
 
-		public virtual AssemblyDefinition Resolve(string fullName)
+		public virtual AssemblyDefinition Resolve (string fullName)
 		{
-			return Resolve(AssemblyNameReference.Parse(fullName));
+			return Resolve (AssemblyNameReference.Parse (fullName));
 		}
 
-		public virtual AssemblyDefinition Resolve(AssemblyNameReference name)
+		public virtual AssemblyDefinition Resolve (AssemblyNameReference name)
 		{
-			if (name.Name == "mscorlib") // TODO: versions
-				return AssemblyFactory.GetAssembly(typeof(object).Module.FullyQualifiedName);
-			else if (IsInGac(name))
-				return AssemblyFactory.GetAssembly(GetFromGac(name));
+			if (name.Name == "mscorlib")
+				return GetCorlib (name);
+			else if (IsInGac (name))
+				return AssemblyFactory.GetAssembly (GetFromGac (name));
 
-			string[] exts = new string[] { ".dll", ".exe" };
-			string[] dirs = new string[] { ".", "bin" };
+			string [] exts = new string [] { ".dll", ".exe" };
+			string [] dirs = new string [] { ".", "bin" };
 
-			foreach (string dir in dirs)
-			{
-				foreach (string ext in exts)
-				{
-					string file = Path.Combine(dir, name.Name + ext);
-					if (File.Exists(file))
-						return AssemblyFactory.GetAssembly(file);
+			foreach (string dir in dirs) {
+				foreach (string ext in exts) {
+					string file = Path.Combine (dir, name.Name + ext);
+					if (File.Exists (file))
+						return AssemblyFactory.GetAssembly (file);
 				}
 			}
 
-			throw new FileNotFoundException("Could not resolve: " + name);
+			throw new FileNotFoundException ("Could not resolve: " + name);
 		}
 
-		static bool IsInGac(AssemblyNameReference reference)
+		AssemblyDefinition GetCorlib (AssemblyNameReference reference)
+		{
+			SR.AssemblyName corlib = typeof (object).Assembly.GetName ();
+			if (corlib.Version == reference.Version)
+				return AssemblyFactory.GetAssembly (typeof (object).Module.FullyQualifiedName);
+
+			string path = Directory.GetParent (
+				Directory.GetParent (
+					typeof (object).Module.FullyQualifiedName).FullName
+				).FullName;
+
+			if (OnMono ()) {
+				if (reference.Version.Major == 1)
+					path = Path.Combine (path, "1.0");
+				else if (reference.Version.Major == 2)
+					path = Path.Combine (path, "2.0");
+				else
+					throw new NotSupportedException ("Version not supported: " + reference.Version);
+			} else {
+				if (reference.Version.ToString () == "1.0.3300.0")
+					path = Path.Combine (path, "v1.0.3705");
+				else if (reference.Version.ToString () == "1.0.5000.0")
+					path = Path.Combine (path, "v1.1.4322");
+				else if (reference.Version.ToString () == "2.0.0.0")
+					path = Path.Combine (path, "v2.0.50727");
+				else
+					throw new NotSupportedException ("Version not supported: " + reference.Version);
+			}
+
+			return AssemblyFactory.GetAssembly (Path.Combine (path, "mscorlib.dll"));
+		}
+
+		bool OnMono ()
+		{
+			return typeof (object).Assembly.GetType ("System.MonoType", false) != null;
+		}
+
+		static bool IsInGac (AssemblyNameReference reference)
 		{
 			if (reference.PublicKeyToken == null || reference.PublicKeyToken.Length == 0)
 				return false;
 
-			return File.Exists(GetFromGac(reference));
+			return File.Exists (GetFromGac (reference));
 		}
 
-		static string GetFromGac(AssemblyNameReference reference)
+		static string GetFromGac (AssemblyNameReference reference)
 		{
-			StringBuilder sb = new StringBuilder();
-			sb.Append(reference.Version);
-			sb.Append("__");
+			StringBuilder sb = new StringBuilder ();
+			sb.Append (reference.Version);
+			sb.Append ("__");
 			for (int i = 0; i < reference.PublicKeyToken.Length; i++)
-				sb.Append(reference.PublicKeyToken[i].ToString("x2"));
+				sb.Append (reference.PublicKeyToken [i].ToString ("x2"));
 
-			return Path.Combine(
-				Path.Combine(
-					Path.Combine(GetGacPath(), reference.Name), sb.ToString()),
-					string.Concat(reference.Name, ".dll"));
+			return Path.Combine (
+				Path.Combine (
+					Path.Combine (GetGacPath (), reference.Name), sb.ToString ()),
+					string.Concat (reference.Name, ".dll"));
 		}
 
-		static string GetGacPath()
+		static string GetGacPath ()
 		{
-			return Directory.GetParent(
-				Directory.GetParent(
-					Path.GetDirectoryName(
-						typeof(Uri).Module.FullyQualifiedName)
+			return Directory.GetParent (
+				Directory.GetParent (
+					Path.GetDirectoryName (
+						typeof (Uri).Module.FullyQualifiedName)
 					).FullName
 				).FullName;
 		}
