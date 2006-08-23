378eb8d9347c68d2e3247f561c91f10c9c1cb0ed
Jb Evain <jbevain@gmail.com>
2006-08-23 12:14:38 +0000

    2006-08-23  Jb Evain  <jbevain@gmail.com>
    	* Mono.Cecil/AssemblyInfo.cs:
    		Cecil 0.4.3 release.
    
    svn path=/trunk/cecil/; revision=64238

--- lib/Mono.Cecil/AssemblyInfo.cs
+++ lib/Mono.Cecil/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant (false)]
 [assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion ("0.4.2.0")]
+[assembly: AssemblyVersion ("0.4.3.0")]
 
 #endif
