0562f97d119145fd29671f1352548ad1778340e8
Sebastien Pouliot <sebastien@ximian.com>
2006-11-07 04:23:04 +0000

    In Test: 2006-11-06  Sebastien Pouliot  <sebastien@ximian.com>
    	* ChangeLog: New. Track changes to UI rules.
    	* ExecutableTargetTest.cs: New. Data (assemblies) for tests.
    	* GtkSharpExecutableTargetTest.cs: New. Tests for Gtk# apps.
    	* Makefile.am: New. Build file.
    	* SystemWindowsFormsExecutableTargetTest.cs: New. Tests for SWF apps.
    	* Test.Rules.Ui.mdp: New. MonoDevelop project file for unit tests.
    In .:
    2006-11-06  Sebastien Pouliot  <sebastien@ximian.com>
    
    	* ChangeLog: New. Track changes to UI rules.
    	* ExecutableTargetRule.cs: New. Abstract rule to detect /target:winexe
    	in GUI applications.
    	* Gendarme.Rules.Ui.mdp: New. MonoDevelop project file for rules.
    	* Gendarme.Rules.Ui.xml.in: New. XML description of the rules.
    	* GtkSharpExecutableTargetRule.cs: New. Rule for Gtk#.
    	* Makefile.am: New. Build file.
    	* SystemWindowsFormsExecutableTargetRule.cs: New. Rule for SWF.
    
    svn path=/trunk/cecil/; revision=67429

---
+++ gendarme/rules/Gendarme.Rules.Ui/ExecutableTargetRule.cs
@@ -0,0 +1,64 @@
+//
+// Gendarme.Rules.Ui.ExecutableTargetRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2006 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Mono.Cecil;
+using Gendarme.Framework;
+
+namespace Gendarme.Rules.Ui {
+
+	abstract public class ExecutableTargetRule : IAssemblyRule {
+
+		abstract protected bool CheckReferences (AssemblyDefinition assembly);
+
+		abstract protected string Toolkit { get; }
+
+		public MessageCollection CheckAssembly (AssemblyDefinition assembly, Runner runner)
+		{
+			// 1. Check entry point, if no entry point then it's not an executable
+			if (assembly.EntryPoint == null)
+				return runner.RuleSuccess;
+
+			// 2. Check if the assembly references SWF
+			if (!CheckReferences (assembly))
+				return runner.RuleSuccess;
+
+			// *** ok, the rule applies! ***
+
+			// 3. On Windows a console window will appear if the subsystem isn't Windows
+			//    i.e. the assembly wasn't compiled with /target:winexe
+			if (assembly.Kind == AssemblyKind.Windows)
+				return runner.RuleSuccess;
+
+			string text = String.Format ("This {0} application was compiled without /target:winexe", Toolkit);
+			Message msg = new Message (text, null, MessageType.Warning);
+			MessageCollection mc = new MessageCollection (msg);
+			return mc;
+		}
+	}
+}
---
+++ gendarme/rules/Gendarme.Rules.Ui/SystemWindowsFormsExecutableTargetRule.cs
@@ -0,0 +1,59 @@
+//
+// Gendarme.Rules.Ui.SystemWindowsFormsExecutableTargetRule
+//
+// Authors:
+//	Sebastien Pouliot <sebastien@ximian.com>
+//
+// Copyright (C) 2006 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using Mono.Cecil;
+using Gendarme.Framework;
+
+namespace Gendarme.Rules.Ui {
+
+	public class SystemWindowsFormsExecutableTargetRule: ExecutableTargetRule {
+
+		protected override bool CheckReferences (AssemblyDefinition assembly)
+		{
+			foreach (AssemblyNameReference a in assembly.MainModule.AssemblyReferences) {
+				// check name and public key token (but not version or culture)
+				if (a.Name == "System.Windows.Forms") {
+					byte[] token = a.PublicKeyToken;
+					if (token != null) {
+						if ((token[0] == 0xb7) && (token[1] == 0x7a) &&
+						    (token[2] == 0x5c) && (token[3] == 0x56) &&
+						    (token[4] == 0x19) && (token[5] == 0x34) &&
+						    (token[6] == 0xe0) && (token[7] == 0x89))
+							return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		protected override string Toolkit {
+			get { return "WinForms"; }
+		}
+	}
+}
