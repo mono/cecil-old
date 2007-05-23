2bbad4ad246851b1c5262e673ae5d61d96383c2a
Jb Evain <jbevain@gmail.com>
2007-05-23 00:19:29 +0000

    2007-05-23  Jb Evain  <jbevain@novell.com>
    	* Mono.Limker/CleanStep.cs:
    		Stop doing hackish stupid things because
    		Cecil suddenly handles MemberRef much better.
    
    svn path=/trunk/cecil/; revision=77821

--- linker/Mono.Linker/CleanStep.cs
+++ linker/Mono.Linker/CleanStep.cs
@@ -43,22 +43,8 @@ namespace Mono.Linker {
 
 		static void CleanAssembly (AssemblyDefinition asm)
 		{
-			CleanMemberReferences (asm.MainModule);
 			foreach (TypeDefinition type in asm.MainModule.Types)
 				CleanType (type);
-		}
-
-		static void CleanMemberReferences (ModuleDefinition module)
-		{
-			foreach (MemberReference reference in new ArrayList (module.MemberReferences)) {
-				GenericInstanceType git = reference.DeclaringType as GenericInstanceType;
-				if (git == null)
-					continue;
-
-				foreach (TypeReference arg in git.GenericArguments)
-					if (!CheckType (module, arg))
-						module.MemberReferences.Remove (reference);
-			}
 		}
 
 		static bool CheckType (ModuleDefinition module, TypeReference reference)
