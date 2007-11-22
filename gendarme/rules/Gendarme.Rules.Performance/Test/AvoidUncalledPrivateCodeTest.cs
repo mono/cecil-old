637fb3354eefc6fdef5f84e5f5ef4ef5db8aede8
Sebastien Pouliot <sebastien@ximian.com>
2007-11-22 21:30:49 +0000

    2007-11-22  Sebastien Pouliot  <sebastien@ximian.com>
    	* AvoidUncalledPrivateCodeTest.cs: Add test case for explicit 
    	interface implementation.
    
    
    svn path=/trunk/cecil/; revision=90194

--- gendarme/rules/Gendarme.Rules.Performance/Test/AvoidUncalledPrivateCodeTest.cs
+++ gendarme/rules/Gendarme.Rules.Performance/Test/AvoidUncalledPrivateCodeTest.cs
@@ -36,6 +36,7 @@ using Gendarme.Framework;
 using Gendarme.Rules.Performance;
 using Mono.Cecil;
 using NUnit.Framework;
+using System.Collections;
 
 namespace Test.Rules.Performance {
 	[TestFixture]
@@ -218,6 +219,79 @@ namespace Test.Rules.Performance {
 			}
 
 			// note: don't add anything else in this class or the test will break
+		}
+
+		public class MyList : IList {
+
+			int IList.Add (object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			void IList.Clear ()
+			{
+				throw new NotImplementedException ();
+			}
+
+			bool IList.Contains (object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			int IList.IndexOf (object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			void IList.Insert (int index, object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			bool IList.IsFixedSize {
+				get { throw new NotImplementedException (); }
+			}
+
+			bool IList.IsReadOnly {
+				get { throw new NotImplementedException (); }
+			}
+
+			void IList.Remove (object value)
+			{
+				throw new NotImplementedException ();
+			}
+
+			void IList.RemoveAt (int index)
+			{
+				throw new NotImplementedException ();
+			}
+
+			object IList.this [int index] {
+				get { throw new NotImplementedException (); }
+				set { throw new NotImplementedException (); }
+			}
+
+			void ICollection.CopyTo (Array array, int index)
+			{
+				throw new NotImplementedException ();
+			}
+
+			int ICollection.Count {
+				get { throw new NotImplementedException (); }
+			}
+
+			bool ICollection.IsSynchronized  {
+				get { throw new NotImplementedException (); }
+			}
+
+			object ICollection.SyncRoot {
+				get { throw new NotImplementedException (); }
+			}
+
+			IEnumerator IEnumerable.GetEnumerator ()
+			{
+				throw new NotImplementedException ();
+			}
 		}
 
 		private IMethodRule methodRule;
@@ -472,6 +546,15 @@ namespace Test.Rules.Performance {
 		{
 			type = GetTest ("ClassWithFinalizer");
 			Assert.IsNull (methodRule.CheckMethod (type.Methods [0], new MinimalRunner ()));
+		}
+
+		[Test]
+		public void CheckExplicitInterfaceImplementation ()
+		{
+			type = GetTest ("MyList");
+			foreach (MethodDefinition method in type.Methods) {
+				Assert.IsNull (methodRule.CheckMethod (method, new MinimalRunner ()), method.Name);
+			}
 		}
 	}
 }
