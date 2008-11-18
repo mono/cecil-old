6af235f430e4685293e2a3d3c00b42416df36f8a
Jb Evain <jbevain@gmail.com>
2008-11-18 16:16:34 +0000

    enough with building them by hand, here is an automated masterinfo generator
    svn path=/trunk/cecil/; revision=119170

---
+++ corcompare/gen-infos.rb
@@ -0,0 +1,215 @@
+require 'ftools'
+
+$gac = 'C:/WINDOWS/assembly/GAC_MSIL'
+$fx =  'C:/WINDOWS/Microsoft.NET/Framework'
+
+$fx1 = File.join $fx, "v1.1.4322"
+$fx2 = File.join $fx, "v2.0.50727"
+$fx3_0 = 'C:/Program Files/Reference Assemblies/Microsoft/Framework/v3.0'
+$fx3_5 = 'C:/Program Files/Reference Assemblies/Microsoft/Framework/v3.5'
+$sl2_0 = 'C:/Program Files/Microsoft Silverlight/2.0.31005.0'
+
+$net_1_1 = [
+	"mscorlib",
+	"System",
+	"System.Data",
+	"System.Data.OracleClient",
+	"System.DirectoryServices",
+	"System.Drawing",
+	"System.Runtime.Remoting",
+	"System.Runtime.Serialization.Formatters.Soap",
+	"System.Security",
+	"System.ServiceProcess",
+	"System.Web",
+	"System.Web.Services",
+	"System.Windows.Forms",
+	"System.Xml",
+	"cscompmgd",
+	"Microsoft.VisualBasic",
+	"",
+	"System.Configuration.Install",
+	"System.Design",
+	"System.Drawing.Design",
+	"System.EnterpriseServices",
+	"System.Management",
+	"System.Messaging"
+]
+
+$net_2_0 = [
+	"mscorlib",
+	"System",
+	"System.Configuration",
+	"System.Data",
+	"System.Data.OracleClient",
+	"System.DirectoryServices",
+	"System.Drawing",
+	"System.Runtime.Remoting",
+	"System.Runtime.Serialization.Formatters.Soap",
+	"System.Security",
+	"System.ServiceProcess",
+	"System.Transactions",
+	"System.Web",
+	"System.Web.Services",
+	"System.Windows.Forms",
+	"System.Xml",
+	"cscompmgd",
+	"Microsoft.VisualBasic",
+	"",
+	"Microsoft.Build.Engine",
+	"Microsoft.Build.Framework",
+	"Microsoft.Build.Tasks",
+	"Microsoft.Build.Utilities",
+	"",
+	"System.Configuration.Install",
+	"System.Design",
+	"System.Drawing.Design",
+	"System.EnterpriseServices",
+	"System.Management",
+	"System.Messaging",
+]
+
+$net_3_0 = [
+	"PresentationCore",
+
+	"PresentationFramework",
+	"System.Speech",
+	"WindowsBase",
+	"",
+	"System.IdentityModel",
+	"System.IdentityModel.Selectors",
+	"System.IO.Log",
+	"System.Runtime.Serialization",
+	"System.ServiceModel",
+	"",
+	"System.Workflow.Activities",
+	"System.Workflow.ComponentModel",
+	"System.Workflow.Runtime",
+	"",
+	"PresentationBuildTasks",
+	"",
+	"PresentationFramework.Aero",
+	"PresentationFramework.Classic",
+	"PresentationFramework.Luna",
+	"PresentationFramework.Royale",
+	"ReachFramework",
+	"",
+	"System.Printing",
+]
+
+$net_3_5 = [
+	"mscorlib",
+	"System",
+	"System.AddIn",
+	"System.AddIn.Contract",
+	"System.Configuration",
+	"System.Core", 
+	"System.Configuration.Install",
+	"System.Data",
+	"System.Data.Linq",
+	"System.Data.OracleClient",
+	"System.DirectoryServices",
+	# "System.DirectoryServices.AccountManagement",
+	# "System.DirectoryServices.Protocols",
+	"System.Drawing",
+	"System.Net",
+	"System.Runtime.Remoting",
+	"System.Security",
+	"System.ServiceProcess",
+	"System.Transactions",
+	"System.Web",
+	"System.Web.Extensions",
+	"System.Web.Extensions.Design",
+	"System.Web.Mobile",
+	"System.Web.RegularExpressions",
+	"System.Web.Services",
+	"System.Windows.Forms",
+	"System.Xml",
+	"System.Xml.Linq",
+	"",
+	"System.Runtime.Serialization.Formatters.Soap",
+	"cscompmgd",
+	"Microsoft.VisualBasic",
+	"",
+	"Microsoft.Build.Engine",
+	"Microsoft.Build.Framework",
+	"Microsoft.Build.Tasks",
+	"Microsoft.Build.Utilities",
+	"",
+	"System.Configuration.Install",
+	"System.Design",
+	"System.Drawing.Design",
+	"System.EnterpriseServices",
+	"System.Management",
+	"System.Management.Instrumentation",
+	"System.Messaging",
+]
+
+$sl_2_0 = [
+	"mscorlib",
+	"System.Windows",
+	"Microsoft.VisualBasic",
+	"System",
+	"System.Core",
+	"System.Net",
+	"System.Runtime.Serialization",
+	"System.ServiceModel",
+	"System.Windows.Browser",
+	"System.Xml",
+]
+
+def locate(assembly, fxs = nil)
+	if fxs
+		fxs.each do |fx|
+			file = File.join fx, assembly + ".dll"
+			return file if File.file?(file)
+		end
+	end
+
+	gac = File.join $gac, assembly, "**", "*.dll"
+	
+	glob = Dir.glob gac
+
+	return glob.first if glob and glob.length > 0
+end
+
+def clean(profile)
+	if not File.directory? profile
+		Dir.mkdir(profile)
+		return
+	end
+
+	files = File.join profile, "*"
+	Dir.glob(files).each do |file|
+		File.delete file
+	end
+end
+
+def generate(profile, assembly)
+	asm = File.join profile, assembly + ".dll"
+	out = File.join profile, assembly + ".xml"
+	system("./mono-api-info.exe #{asm} > #{out}")
+end
+
+def process(profile, assemblies, fxs = nil)
+	clean(profile)
+
+	assemblies.each do |assembly|
+		if assembly != nil and assembly.length > 0
+			#puts assembly 
+			location = locate(assembly, fxs)
+			if location
+				File.copy(location, profile)
+				generate(profile, assembly)
+			else
+				puts "fail to locate " + assembly
+			end
+			#puts "   " + location if location
+		end
+	end
+end
+
+process("1.1", $net_1_1, [$fx1])
+#process("2.0", $net_2_0, [$fx2])
+process("3.0", $net_3_0, [$fx3_0, $fx2])
+process("3.5", $net_3_5, [$fx3_5, $fx2])
+process("SL2", $sl_2_0, [$sl2_0])
