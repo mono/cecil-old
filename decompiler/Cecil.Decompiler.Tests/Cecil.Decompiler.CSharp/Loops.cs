d95e29e9c12c6fe4c979dab9108e1c5dfe84f641
Jb Evain <jbevain@gmail.com>
2009-02-14 17:12:17 +0000

    port loops tests
    svn path=/trunk/cecil/; revision=126934

---
+++ decompiler/Cecil.Decompiler.Tests/Cecil.Decompiler.CSharp/Loops.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using Cecil.Decompiler.Languages;
+
+namespace Cecil.Decompiler.CSharp {
+
+	[TestFixture]
+	public class Loops {
+
+		[CSharp (Version = CSharpVersion.V1)]
+		public void SimpleFor ()
+		{
+		}
+
+		[CSharp (Version = CSharpVersion.V1)]
+		public void SimpleDoWhile ()
+		{
+		}
+
+		[CSharp (Version = CSharpVersion.V1)]
+		public void SimpleWhile ()
+		{
+		}
+
+		[CSharp (Version = CSharpVersion.V1)]
+		public void NestedDoWhile ()
+		{
+		}
+
+		[CSharp (Version = CSharpVersion.V1)]
+		public void BreakInWhile ()
+		{
+		}
+	}
+}
---
+++ decompiler/Cecil.Decompiler.Tests/TestCases/CSharp/Loops.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program {
+
+	public void SimpleFor (int a)
+	{
+		for (int i = 0; i < 10; i++) {
+			a++;
+		}
+	}
+
+	public void SimpleDoWhile (int a)
+	{
+		Console.WriteLine (a);
+		do {
+			a++;
+			Console.WriteLine (a);
+		} while (a < 100);
+		Console.WriteLine (a);
+	}
+
+	public void SimpleWhile (int a)
+	{
+		Console.WriteLine (a);
+		while (a < 100) {
+			a++;
+			Console.WriteLine (a);
+		}
+		Console.WriteLine (a);
+	}
+
+	public void NestedDoWhile (int a)
+	{
+		while (a < 100) {
+			do {
+				a *= 10;
+			} while (a < 10);
+			a += 2;
+		}
+	}
+
+	public void BreakInWhile (int a)
+	{
+		while (a < 100) {
+			if (a == 12) {
+				Console.WriteLine (a);
+				break;
+			} else
+				Console.WriteLine (a);
+
+			a--;
+		}
+		Console.WriteLine (a);
+	}
+}
