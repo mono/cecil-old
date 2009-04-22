bf066eb6f06679b09b417cece7e030b871f41201
Jb Evain <jbevain@gmail.com>
2009-04-22 18:39:46 +0000

    add prototypes
    svn path=/trunk/cecil/; revision=132392

---
+++ decompiler/dbg/FooVisitor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+using Cecil.Decompiler;
+using Cecil.Decompiler.Ast;
+
+interface IFooExpression {
+	FooExpressionType FooExpressionType { get; }
+}
+
+enum FooExpressionType {
+	Foo,
+	Bar
+}
+
+class FooExpression : Expression, IFooExpression {
+
+	public override CodeNodeType CodeNodeType {
+		get { return (CodeNodeType) - 1; } // should actually be CodeNodeType.None or Unknown
+	}
+
+	public FooExpressionType FooExpressionType {
+		get { return FooExpressionType.Foo; }
+	}
+}
+
+class BarExpression : Expression, IFooExpression {
+
+	public override CodeNodeType CodeNodeType {
+		get { return (CodeNodeType) - 1; }
+	}
+
+	public FooExpressionType FooExpressionType {
+		get { return FooExpressionType.Bar; }
+	}
+}
+
+class FooVisitor : BaseCodeVisitor {
+
+	public override void Visit (ICodeNode node)
+	{
+		var foo_node = node as IFooExpression;
+		if (foo_node == null) {
+			base.Visit (node);
+			return;
+		}
+
+		switch (foo_node.FooExpressionType) {
+		case FooExpressionType.Foo:
+			VisitFoo ((FooExpression) node);
+			break;
+		case FooExpressionType.Bar:
+			VisitBar ((BarExpression) node);
+			break;
+		}
+	}
+
+	public virtual void VisitFoo (FooExpression node)
+	{
+	}
+
+	public virtual void VisitBar (BarExpression node)
+	{
+	}
+}
+
+class FooPrinter : FooVisitor {
+
+	public override void VisitAssignExpression (AssignExpression node)
+	{
+		Console.Write ("assign ");
+
+		Visit (node.Expression);
+
+		Console.Write (" to ");
+
+		Visit (node.Target);
+
+		Console.WriteLine ();
+	}
+
+	public override void VisitFoo (FooExpression node)
+	{
+		Console.Write ("Foo");
+	}
+
+	public override void VisitBar (BarExpression node)
+	{
+		Console.Write ("Bar");
+	}
+}
+
+class Test {
+
+	public static void Main ()
+	{
+		var assign = new AssignExpression (new BarExpression (), new FooExpression ());
+
+		var printer = new FooPrinter ();
+
+		printer.Visit (assign);
+	}
+}
---
+++ decompiler/dbg/Linq.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SLE = System.Linq.Expressions;
+using System.Reflection;
+
+using Cecil.Decompiler;
+using Cecil.Decompiler.Ast;
+using Cecil.Decompiler.Languages;
+
+using Mono.Cecil;
+
+namespace Cecil.Decompiler.Linq {
+
+	class ExpressionConverter : BaseCodeVisitor {
+
+		SLE.Expression [] parameters;
+		MethodDefinition method;
+		Stack<SLE.Expression> expressions = new Stack<SLE.Expression> ();
+
+		private ExpressionConverter (SLE.Expression [] parameters, MethodDefinition method)
+		{
+			this.parameters = parameters;
+			this.method = method;
+		}
+
+		void Push (SLE.Expression expression)
+		{
+			expressions.Push (expression);
+		}
+
+		SLE.Expression Pop ()
+		{
+			return expressions.Pop ();
+		}
+
+		public override void VisitBinaryExpression (BinaryExpression node)
+		{
+			Visit (node.Right);
+			Visit (node.Left);
+
+			Push (SLE.Expression.MakeBinary (
+				ConvertBinaryType (node.Operator),
+				Pop (),
+				Pop ()));
+		}
+
+		public override void VisitLiteralExpression (LiteralExpression node)
+		{
+			Push (SLE.Expression.Constant (node.Value));
+		}
+
+		public override void VisitArgumentReferenceExpression (ArgumentReferenceExpression node)
+		{
+			Push (parameters [method.Parameters.IndexOf ((ParameterDefinition) node.Parameter)]);
+		}
+
+		SLE.ExpressionType ConvertBinaryType (BinaryOperator @operator)
+		{
+			switch (@operator) {
+			case BinaryOperator.Add:
+				return SLE.ExpressionType.Add;
+			case BinaryOperator.Multiply:
+				return SLE.ExpressionType.Multiply;
+			default:
+				throw new NotImplementedException ();
+			}
+		}
+
+		public static SLE.Expression ConvertMethod (SLE.Expression [] parameters, MethodDefinition method)
+		{
+			var body = method.Body.Decompile (CSharp.GetLanguage (CSharpVersion.V1));
+
+			if (body.Statements.Count != 1)
+				throw new ArgumentException ();
+
+			var @return = body.Statements [0] as ReturnStatement;
+			if (@return == null)
+				throw new ArgumentException ();
+
+			var converter = new ExpressionConverter (parameters, method);
+			converter.Visit (@return.Expression);
+			return converter.Pop ();
+		}
+	}
+
+	static class DelegateConverter {
+
+		public static SLE.Expression<TDelegate> ToExpression<TDelegate> (TDelegate @delegate) where TDelegate : class
+		{
+			if (@delegate == null)
+				throw new ArgumentNullException ();
+
+			if (!typeof (Delegate).IsAssignableFrom (typeof (TDelegate)))
+				throw new ArgumentException ();
+
+			var dlg = @delegate as Delegate;
+
+			return ToExpression<TDelegate> (dlg);
+		}
+
+		static SLE.Expression<TDelegate> ToExpression<TDelegate> (Delegate @delegate)
+		{
+			var parameters = CreateParameters (@delegate.Method.GetParameters ());
+			var method = GetMethodDefinition (@delegate.Method);
+
+			return SLE.Expression.Lambda<TDelegate> (
+				ExpressionConverter.ConvertMethod (parameters, method),
+				parameters);
+		}
+
+		static SLE.ParameterExpression [] CreateParameters (ParameterInfo [] originals)
+		{
+			var parameters = new SLE.ParameterExpression [originals.Length];
+			for (int i = 0; i < originals.Length; i++)
+				parameters [i] = SLE.Expression.Parameter (originals [i].ParameterType, originals [i].Name);
+
+			return parameters;
+		}
+
+		static MethodDefinition GetMethodDefinition (MethodInfo original)
+		{
+			TypeDefinition type = GetTypeDefinition (original.DeclaringType);
+
+			var matches = from MethodDefinition meth in type.Methods
+						  where MethodMatch (meth, original)
+						  select meth;
+
+			return matches.FirstOrDefault ();
+		}
+
+		static AssemblyDefinition GetAssembly (Assembly assembly)
+		{
+			return AssemblyFactory.GetAssembly (assembly.ManifestModule.FullyQualifiedName);
+		}
+
+		static string GetFullName (Type type)
+		{
+			if (type.DeclaringType != null)
+				return type.FullName.Replace ('+', '/');
+
+			return type.FullName;
+		}
+
+		static TypeDefinition GetTypeDefinition (Type type)
+		{
+			var assembly = GetAssembly (type.Assembly);
+			return assembly.MainModule.Types [GetFullName (type)];
+		}
+
+		static bool ParameterMatch (ParameterDefinition parameter, ParameterInfo info)
+		{
+			return parameter.ParameterType.FullName == GetFullName (info.ParameterType);
+		}
+
+		static bool ParametersMatch (ParameterDefinitionCollection parameters, ParameterInfo [] infos)
+		{
+			if (parameters.Count != infos.Length)
+				return false;
+
+			for (int i = 0; i < parameters.Count; i++)
+				if (!ParameterMatch (parameters [i], infos [i]))
+					return false;
+
+			return true;
+		}
+
+		static bool MethodMatch (MethodDefinition method, MethodInfo info)
+		{
+			if (method.Name != info.Name)
+				return false;
+			if (method.ReturnType.ReturnType.Name != info.ReturnType.Name)
+				return false;
+
+			return ParametersMatch (method.Parameters, info.GetParameters ());
+		}
+	}
+
+	class Program {
+
+		static void Main ()
+		{
+			Func<int, int> magic = i => i * 42;
+
+			SLE.Expression<Func<int, int>> exp = DelegateConverter.ToExpression (magic);
+
+			Console.WriteLine (exp.ToString ());
+			Console.WriteLine (exp.Compile ().Invoke (1));
+		}
+	}
+}
